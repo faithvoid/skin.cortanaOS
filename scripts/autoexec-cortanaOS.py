30b465a6b4daea46cc2ff10aedde9a0b4b631fd4
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2025-07-24 19:47:15 -0400

    Update autoexec-cortanaOS.py

--- scripts/autoexec-cortanaOS.py
+++ scripts/autoexec-cortanaOS.py
@@ -2,9 +2,9 @@ import xbmc
 
 # Only proceed if a network connection is detected
 if xbmc.getCondVisibility("System.HasNetwork"):
-    xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/serverbrowser/getnews.py)")
-    xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/serverbrowser/playercount.py)")
-    # xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/startup/mediapresence.py)")
-    # xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/chat/notifier.py)")
+    xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/serverbrowser/getnews.py)") # Fetches Insignia & XLink Kai News & Events
+    xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/serverbrowser/playercount.py)") # Fetches Insignia & XLink Kai player counts
+    # xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/startup/sakuraMedia.py)") # Xbox Discord Rich Presence for media
+    # xbmc.executebuiltin("XBMC.RunScript(special://skin/scripts/chat/notifier.py)") # cortanaChat notifications
 else:
     xbmc.log("Network not available, skipping startup scripts.", xbmc.LOGNOTICE)
